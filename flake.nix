33218764b0b7c671e506f6a810945642e7b84421
haruki7049 <tontonkirikiri@gmail.com>
2024-12-21 00:03:56 +0900

    feat(nix-flakes): Init

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.perltidy.enable = true;
+            settings.formatter.perltidy = {
+              #perltidyrc = ./.perltidyrc;
+              includes = [
+                "*.pl"
+                "*.PL"
+                "*.pm"
+                "*.PM"
+              ];
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.perl
+            ];
+          };
+        };
+    };
+}
